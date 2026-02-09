ed9826f42906f666b28014df51f1b5ef17ffc885
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-09 17:50:02 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -42,7 +42,7 @@ if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
     VALIDATE $? "Creating System User"
 else
-    echo -e "Roboshop user already exists... $Y Skipping $N"
+    echo -e "Roboshop user already exists... $Y SKIPPING $N"
 fi
 
 mkdir -p /app 
