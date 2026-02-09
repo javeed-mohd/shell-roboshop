32a780e4db91e5b215ae96c497de684562c3c79d
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-09 19:34:10 +0530

    actions

---
+++ user.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# To log files
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop" # shell-roboshop --> folder name
+LOGS_FILE="$LOGS_FOLDER/$0.log"  # $0=12-logs.sh(script name)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.devopsdaws.online
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1 
+fi
+
+mkdir -p $LOGS_FOLDER 
+
+# By default shell will not execute this, only it will be executed when called (It can be validate, status_check...)
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+        echo -e "$2... $R FAILURE $N" | tee -a $LOGS_FILE # Tee is used for showing the output both in terminal and file (logs_file)
+        exit 1
+    else 
+        echo -e "$2... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+
+}
+
+dnf module disable nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Disabling NodeJS default version"
+
+dnf module enable nodejs:20 -y &>> $LOGS_FILE
+VALIDATE $? "Enabling NodeJS 20"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Install NodeJS"
+
+id roboshop &>> $LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
+    VALIDATE $? "Creating System User"
+else
+    echo -e "Roboshop user already exists... $Y SKIPPING $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "Creating App Directory"
+
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>> $LOGS_FILE
+VALIDATE $? "Downloading User Code"
+
+cd /app 
+VALIDATE $? "Moving to App Directory"
+
+rm -rf /app/* # It removes all the content which is present in that folder
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/user.zip &>> $LOGS_FILE
+VALIDATE $? "Unzip User Code"
+
+npm install &>> $LOGS_FILE
+VALIDATE $? "Installing Dependencies"
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+VALIDATE $? "Created systemctl service"
+
+systemctl daemon-reload
+systemctl enable user &>> $LOGS_FILE
+systemctl start user
+VALIDATE $? "Enabling and Starting User"
