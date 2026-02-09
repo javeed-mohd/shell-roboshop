fd2821044fbd7aee82e868a77d85764f6624e53c
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-09 18:39:34 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -9,6 +9,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.devopsdaws.online
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
@@ -71,3 +72,9 @@ systemctl daemon-reload
 systemctl enable catalogue &>> $LOGS_FILE
 systemctl start catalogue
 VALIDATE $? "Enabling and Starting Catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y
+
+# INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+mongosh --host $MONGODB_HOST </app/db/master-data.js
