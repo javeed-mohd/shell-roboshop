7fa4eadfa2f3b4c704501c745aa68b672e2fc4bb
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-09 17:48:55 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -37,10 +37,15 @@ VALIDATE $? "Enabling NodeJS 20"
 dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "Install NodeJS"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
-VALIDATE $? "Creating System User"
+id roboshop &>> $LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
+    VALIDATE $? "Creating System User"
+else
+    echo -e "Roboshop user already exists... $Y Skipping $N"
+fi
 
-mkdir /app 
+mkdir -p /app 
 VALIDATE $? "Creating App Directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> $LOGS_FILE
