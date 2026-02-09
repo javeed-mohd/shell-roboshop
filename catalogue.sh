f0c26ef3b4cefd81b1dbdb03fc9303ccc3288ae0
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-09 17:38:04 +0530

    actions

---
+++ catalogue.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# To log files
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop" # shell-roboshop --> folder name
+LOGS_FILE="$LOGS_FOLDER/$0.log"  # $0=12-logs.sh(script name)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1 
+fi
+
+mkdir -p $LOGS_FOLDER 
+
+# By default shell will not execute this, only it will be executed when called (It can be validate, status_check...)
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+        echo -e "$2... $R FAILURE $N" | tee -a $LOGS_FILE # Tee is used for showing the output both in terminal and file (logs_file)
+        exit 1
+    else 
+        echo -e "$2... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+
+}
+
+dnf module disable nodejs -y &>> $LOGS_FILE
+VALIDATE $? ("Disabling NodeJS default version")
+
+dnf module enable nodejs:20 -y &>> $LOGS_FILE
+VALIDATE $? ("Enabling NodeJS 20")
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? ("Install NodeJS")
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
+VALIDATE $? ("Creating System User")
+
+mkdir /app 
+VALIDATE $? ("Creating App Directory")
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>> $LOGS_FILE
+VALIDATE $? ("Downloading Catalogue Code")
