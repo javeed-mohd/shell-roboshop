a229ba9593c99beb9da106358e9fedca341bf27b
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-10 14:14:12 +0530

    actions

--- frontend.sh
+++ frontend.sh
@@ -42,9 +42,9 @@ VALIDATE $? "Enabling and Starting Nginx"
 rm -rf /usr/share/nginx/html/* 
 VALIDATE $? "Removing Default Content"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $LOGS_FILE
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>> $LOGS_FILE
 VALIDATE $? "Downloaing and Unzipping Frontend"
 
 rm -rf /etc/nginx/nginx.conf
