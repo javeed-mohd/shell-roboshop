9cdf7cf2930cec5e3255da90804341058c127cc4
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-10 11:51:12 +0530

    actions

---
+++ mysql.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# To log files
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop" # shell-roboshop --> folder name
+LOGS_FILE="$LOGS_FOLDER/$0.log"  # $0=12-logs.sh(script name)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1 
+fi
+
+mkdir -p $LOGS_FOLDER 
+
+# By default shell will not execute this, only it will be executed when called (It can be validate, status_check...)
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+        echo -e "$2... $R FAILURE $N" | tee -a $LOGS_FILE # Tee is used for showing the output both in terminal and file (logs_file)
+        exit 1
+    else 
+        echo -e "$2... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+
+}
+
+dnf install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>> $LOGS_FILE
+systemctl start mysqld
+VALIDATE $? "Enabling and Starting MySQL"
+
+# Get the password from user
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setup root password"
