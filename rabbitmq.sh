5dcbd7fcc64519f512b6a72f320be421e4bfeb4d
Javeed Mohammed <javeedmohd214@gmail.com>
2026-02-10 13:01:18 +0530

    actions

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -41,6 +41,6 @@ systemctl enable rabbitmq-server &>> $LOGS_FILE
 systemctl start rabbitmq-server
 VALIDATE $? "Enabling and Starting RabbitMQ"
 
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop roboshop123 &>> $LOGS_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGS_FILE
 VALIDATE $? "Created User and given permissions"
